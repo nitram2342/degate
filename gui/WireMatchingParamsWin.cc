f661a64bae2a189afc0452f3b73556d1f9802a57
xorrhks0216 <taekgwankim@gmail.com>
2013-03-09 13:04:15 +0900

    Add copyright

--- gui/WireMatchingParamsWin.cc
+++ gui/WireMatchingParamsWin.cc
@@ -3,6 +3,7 @@
 This file is part of the IC reverse engineering tool degate.
 
 Copyright 2008, 2009, 2010 by Martin Schobert
+Copyright 2013 by Taekgwan Kim
 
 Degate is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
